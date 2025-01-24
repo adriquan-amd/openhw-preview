1c23c1d9c5e7f7c63299e7930710f1d1d9bbb6cd
ZenXingyhun <quanxingrun@gmail.com>
2025-01-25 01:10:49 +0800

    Update posters

--- scripts/home_scripts.js
+++ scripts/home_scripts.js
@@ -3,11 +3,20 @@ $(document).ready(function () {
       // 移除其他卡片的高亮
       $(".card").removeClass("highlight");
       // 给当前悬停的卡片添加高亮
-      $(this).addClass("highlight");
+      $(this).addClass("highlight  overlay-black");
+      const imgSrc = $(this).find(".card-img").attr("src"); 
+      const pdfSrc = imgSrc.replace(".jpg", ".pdf"); 
+
+      if ($(this).find(".hover-btn").length === 0) {
+        $(this)
+          .find(".card-body")
+          .append(`<a href="${pdfSrc}" class="btn btn-outline-white btn-round mt-3 hover-btn" data-aos="slide-up">View</a>`);
+      }
     });
   
     $(".card").on("mouseleave", function () {
       // 移除当前卡片的高亮
-      $(this).removeClass("highlight");
+      $(this).removeClass("highlight overlay-black");
+      $(this).find(".hover-btn").remove();
     });
   });
